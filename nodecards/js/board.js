3c7fc8f1ef58aeef2c7382f0f40420451cea25ea
fpumir <fpumir@terredeweb.fr>
2013-11-21 09:29:44 +0100

    node cards
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

---
+++ nodecards/js/board.js
@@ -0,0 +1,42 @@
+var board={
+  init : function(){
+		if(io==="undefined"){
+			console.log('no socket'); 
+		}
+    this.socket = io.connect('http://macbook-maraboutee.local:8080');
+		this.listen();
+    return this;
+  },
+
+	getAll : function(){
+		this.socket.emit('init');
+		this.socket.on('initialized',function(datas){
+      for (i in datas){
+        board._render(datas[i]);
+      } 
+    });
+		return this; 
+	},
+  
+  listen : function(){
+    this.socket.on('recorded',function(datas){
+      board._render(datas);
+    });
+		return this; 
+  },
+  
+  _render : function(datas){
+    var div=$('<div>').addClass('card');
+		var textSpan=$('<span>').addClass('text').html(datas.title);
+		$('#board').append(div.append(textSpan));
+  },
+  
+}
+board.init().getAll();
+
+$('#addCard').on('submit',function(e){
+  e.preventDefault();
+  var datas={title:$('input[name=name]').val()};
+  board.socket.emit('card', datas);
+  $('input[name=name]').val('');
+});
